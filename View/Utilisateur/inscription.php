f9e98fe2796608a664356510f26212636e278a75
Salimou95 <salimoudiaby95@gmail.com>
2024-04-02 12:02:22 +0200

    Mise à jour des fichiers

--- View/Utilisateur/inscription.php
+++ View/Utilisateur/inscription.php
@@ -9,38 +9,32 @@
     <section class="sectionForm">
         <div>
             <form method="post">
-            <article>
-                <label for="sexeUtilisateur" class="labeform">Genre :</label>
+
+            <label for="sexeUtilisateur" class="labeform">Genre :</label>
                 <select name="sexeUtilisateur" id="sexeUtilisateur" class="inputform" <?= $mode == "suppression" ? "disabled" : "" ?>>
                     <option value="F" <?= ($utilisateur->getSexeUtilisateur() === 'F') ? 'selected' : ''; ?>>Mme</option>
                     <option value="M" <?= ($utilisateur->getSexeUtilisateur() === 'M') ? 'selected' : ''; ?>>Mr</option>
                 </select>
-                </article>
-                <article>
-                    <label for="nomUtilisateur" class="labeform">Nom :</label>
+
+                <label for="nomUtilisateur" class="labeform">Nom :</label>
                     <input type="text" name="nomUtilisateur" id="nomUtilisateur" placeholder="Inscrivez votre nom" class="inputform" value="<?= htmlspecialchars($utilisateur->getNomUtilisateur()) ?>" <?= $mode == "suppression" ? "disabled" : "" ?>>
-                </article>
-                <article>
+
                     <label for="prenomUtilisateur" class="labeform">Prénom :</label>
                     <input type="text" placeholder="Inscrivez votre prenom" name="prenomUtilisateur" id="prenomUtilisateur" class="inputform" value="<?= htmlspecialchars($utilisateur->getprenomUtilisateur())?>" <?= $mode == "suppression" ? "disabled" : "" ?>>
-                </article>
-                <article>
+
                     <label for="emailUtilisateur" class="labeform">Email :</label>
                     <input type="email" name="emailUtilisateur" id="emailUtilisateur" class="inputform" placeholder="Inscrivez votre email" value="<?= htmlspecialchars($utilisateur->getEmailUtilisateur())?>" <?= $mode == "suppression" ? "disabled" : "" ?>>
-                </article>
-                <article>
+
                     <label for="mdpUtilisateur" class="labeform <?= $mode !== "insertion" ? "none" : "" ?>">Mot de passe :</label>
                     <input type="password" name="mdpUtilisateur" id="mdpUtilisateur" class="inputform <?= $mode !== "insertion" ? "none" : "" ?>" placeholder="Inscrivez votre mot de passe" >
-                </article>
-                <article>                            
+
                     <label for="dateNaissanceUtilisateur" class="labeform">Date de naissance :</label>
                     <input type="date" name="dateNaissanceUtilisateur" id="dateNaissanceUtilisateur" class="inputform"<?= $mode == "suppression" ? "disabled" : "" ?> value="<?= htmlspecialchars($utilisateur->getDateNaissanceUtilisateur())?>">
-                </article>
-                <article>
+
                     <label for="telephoneUtilisateur" class="labeform">Telephone :</label>
                     <input type="number" name="telephoneUtilisateur" id="telephoneUtilisateur" class="inputform" placeholder="Inscrivez votre téléphone" value="<?= htmlspecialchars($utilisateur->getTelephoneUtilisateur()) ?>" <?= $mode == "suppression" ? "disabled" : "" ?>>
-                </article>
-                   
+
+                    
                 <article class="subconlinkbtn">
                     <input type="submit" value="<?= $mode == "suppression" ? "Supprimer" : ($mode == "modification" ? "Modifier" : "S'incrire") ?>">
                     <a href="<?= addLink("utilisateur", "connexion")?>" 
